b6be109f87f23ddabc6a17ead7b03abbcbd582aa
mobinzk <mobin.zk@3men.co.uk>
2014-11-24 08:48:00 +0000

    - username deleted
    Signed-off-by: mobinzk <mobin.zk@3men.co.uk>

--- application/modules/root/models/twitter.php
+++ application/modules/root/models/twitter.php
@@ -6,7 +6,6 @@
         public $api_secret = 'S4amGqEyx8pgrivP98IQmB423lq31AUPd5O5DDwRrPslmZtSmY';
         public $access_token = '86920126-RyW0KDWH9TZJKer1erWq0aXSkI7fJx4888iGDbqSR';
         public $access_token_secret = 'sC74kQI9aCo5WRl2gFljf2R5wOFhmnBI3HNJsGOLvG5qv';
-        public $username = 'zadatech';
 
         public function getTweets() {
             $twitter = new TwitterRestApi($this->api_key, $this->api_secret);

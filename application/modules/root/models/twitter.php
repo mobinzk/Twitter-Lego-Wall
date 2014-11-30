c549b97ccdf19ac837de11e91425906f4839ebda
mobinzk <mobin.zk@3men.co.uk>
2014-11-30 12:27:24 +0000

    - Readme updated - Twitter api instruction text added
    Signed-off-by: mobinzk <mobin.zk@3men.co.uk>

--- application/modules/root/models/twitter.php
+++ application/modules/root/models/twitter.php
@@ -2,16 +2,17 @@
 
     Class Twitter {
 
-        public $api_key = 'wWyzn2SsuxX7MUd0jnFLDEL9E';
-        public $api_secret = 'S4amGqEyx8pgrivP98IQmB423lq31AUPd5O5DDwRrPslmZtSmY';
-        public $access_token = '86920126-RyW0KDWH9TZJKer1erWq0aXSkI7fJx4888iGDbqSR';
-        public $access_token_secret = 'sC74kQI9aCo5WRl2gFljf2R5wOFhmnBI3HNJsGOLvG5qv';
+        public $api_key             = 'API KEY';
+        public $api_secret          = 'API SECRET';
+        public $access_token        = 'ACCESS TOCKEN';
+        public $access_token_secret = 'ACCESS TOCKEN SECRET';
+        public $term                = '#sotoncreatives';
 
         public function getTweets() {
             $twitter = new TwitterRestApi($this->api_key, $this->api_secret);
             $twitter->setAccessToken($this->access_token, $this->access_token_secret);
             return $twitter->get('/search/tweets.json', 
-                                    ['q' => '#sotoncreatives',
+                                    ['q' => $this->term,
                                     'exclude' => 'retweets']
                                 );
         }
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Twitter Lego wall
+This app has been created specifically for SotonCreatives lego event, to show all tweets about the event in lego typeface.
+
+## How to use
+To use this app, first of all you need to create an app in [twitter apps](https://apps.twitter.com/).
+Then open twitter.php in /application/modules/root/models
+and update API KEY, API SECRET, ACCESS TOCKEN, ACCESS TOCKEN SECRET and the search term.
+
+enjoy!
+
+### Credits
+The LEGO Group owns the lego blocks and the terms "Lego" belongs to this company
+
+Typography creatively created by Dan Benham @danblahblahblah
+
+The app uses [Pocket framework](https://github.com/philmmoore/Pocket) and [PHPTwitterRestApi](https://github.com/philmmoore/PHPTwitterRestApi) by Philip Moore @philmmoore 
+
+The app developed at [3 Men & a Suit](http://3men.co.uk) by Mobin Zadeh Kochak @mobinzk
+
+Feel free to expand and improve this web app.
